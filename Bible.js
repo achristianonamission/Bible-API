b186978e5f9db5bad966b89e5f89294a3060ec41
achristianonamission <132952751+achristianonamission@users.noreply.github.com>
2023-05-31 20:41:32 -0400

    Bible.js

--- Bible.js
+++ Bible.js
@@ -2,7 +2,7 @@ function load(book)
 {
 if(book==="Genesis"){
 var script=document.createElement("script")
-script.src="https://cdn.jsdelivr.net/gh/achristianonamission/Bible-API@main/Bible.js"
+script.src="https://cdn.jsdelivr.net/gh/achristianonamission/Bible-API@main/Genesis.js"
 document.head.appendChild(script)
 }
 }
