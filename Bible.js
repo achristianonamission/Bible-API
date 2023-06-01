522b613d6f839243a181b4da2f78cd70e334ade8
achristianonamission <132952751+achristianonamission@users.noreply.github.com>
2023-05-31 20:22:46 -0400

    Add files via upload

---
+++ Bible.js
@@ -0,0 +1,8 @@
+function load(book)
+{
+if(book==="Genesis"){
+var script=document.createElement("script")
+script.src="https://cdn.jsdelivr.net/gh/achristianonamission/Bible-API@main/Bible.js"
+document.body.appendChild(script)
+}
+}
