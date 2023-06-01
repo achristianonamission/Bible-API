f5d88ba81c96ee8e275761c16ec1f8800fdb6191
achristianonamission <132952751+achristianonamission@users.noreply.github.com>
2023-05-31 20:18:38 -0400

    Add files via upload

---
+++ Genesis.js
@@ -0,0 +1,101 @@
+var chapter=["Thus the heavens and the earth were finished, and all the host of them.","And on the seventh day God ended his work which he had made; and he rested on the seventh day from all his work which he had made.","And God blessed the seventh day, and sanctified it: because that in it he had rested from all his work which God created and made.","These are the generations of the heavens and of the earth when they were created, in the day that the Lord God made the earth and the heavens,","And every plant of the field before it was in the earth, and every herb of the field before it grew: for the Lord God had not caused it to rain upon the earth, and there was not a man to till the ground.","But there went up a mist from the earth, and watered the whole face of the ground.","And the Lord God formed man of the dust of the ground, and breathed into his nostrils the breath of life; and man became a living soul.","And the Lord God planted a garden eastward in Eden; and there he put the man whom he had formed.","And out of the ground made the Lord God to grow every tree that is pleasant to the sight, and good for food; the tree of life also in the midst of the garden, and the tree of knowledge of good and evil.","And a river went out of Eden to water the garden; and from thence it was parted, and became into four heads.","The name of the first is Pison: that is it which compasseth the whole land of Havilah, where there is gold;","And the gold of that land is good: there is bdellium and the onyx stone.","And the name of the third river is Hiddekel: that is it which goeth toward the east of Assyria. And the fourth river is Euphrates.","And the Lord God took the man, and put him into the garden of Eden to dress it and to keep it.","And the Lord God commanded the man, saying, Of every tree of the garden thou mayest freely eat:","But of the tree of the knowledge of good and evil, thou shalt not eat of it: for in the day that thou eatest thereof thou shalt surely die.","And the Lord God said, It is not good that the man should be alone; I will make him an help meet for him."," And out of the ground the Lord God formed every beast of the field, and every fowl of the air; and brought them unto Adam to see what he would call them: and whatsoever Adam called every living creature, that was the name thereof.","And Adam gave names to all cattle, and to the fowl of the air, and to every beast of the field; but for Adam there was not found an help meet for him.","And the Lord God caused a deep sleep to fall upon Adam, and he slept: and he took one of his ribs, and closed up the flesh instead thereof;","And the rib, which the Lord God had taken from man, made he a woman, and brought her unto the man.","And Adam said, This is now bone of my bones, and flesh of my flesh: she shall be called Woman, because she was taken out of Man."]
+function Genesis(chapter,verse){
+if(chapter===1){
+if(verse===1){
+return "In the beginning God created the heaven and the earth."
+}
+if(verse===2){
+return "And the earth was without form, and void; and darkness was upon the face of the deep."
+}
+if(verse===3){
+return "And God said, Let there be light: and there was light."
+}
+if(verse===4){
+return "And God saw the light, that it was good: and God divided the light from the darkness."
+}
+if(verse===5){
+return "And God called the light Day, and the darkness he called Night. And the evening and the morning were the first day."
+}
+if(verse===6){
+return "And God said, Let there be a firmament in the midst of the waters, and let it divide the waters from the waters."
+}
+if(verse===7){
+return "And God made the firmament, and divided the waters which were under the firmament from the waters which were above the firmament: and it was so."
+}
+if(verse===8){
+return "And God called the firmament Heaven. And the evening and the morning were the second day."
+}
+if(verse===9){
+return "And God said, “Let the water under the sky be gathered to one place, and let dry ground appear.” And it was so."
+}
+if(verse===10){
+return "God called the dry ground “land,” and the gathered waters he called “seas.” And God saw that it was good."
+}
+if(verse===11){
+return "And God said, Let the earth bring forth grass, the herb yielding seed, and the fruit tree yielding fruit after his kind, whose seed is in itself, upon the earth: and it was so."
+}
+if(verse===12){
+return " And the earth brought forth grass, and herb yielding seed after his kind, and the tree yielding fruit, whose seed was in itself, after his kind: and God saw that it was good."
+}
+if(verse===13){
+return "And the evening and the morning were the third day."
+}
+if(verse===14){
+return "And God said, Let there be lights in the firmament of the heaven to divide the day from the night; and let them be for signs, and for seasons, and for days, and years:"
+}
+if(verse===15){
+return "And let them be for lights in the firmament of the heaven to give light upon the earth: and it was so."
+}
+if(verse===16){
+return "And God made two great lights; the greater light to rule the day, and the lesser light to rule the night: he made the stars also."
+}
+if(verse===17){
+return "And God set them in the firmament of the heaven to give light upon the earth,"
+}
+if(verse===18){
+return "And to rule over the day and over the night, and to divide the light from the darkness: and God saw that it was good."
+}
+if(verse===19){
+return "And the evening and the morning were the fourth day."
+}
+if(verse===20){
+return "And God said, Let the waters bring forth abundantly the moving creature that hath life, and fowl that may fly above the earth in the open firmament of heaven."
+}
+if(verse===21){
+return "And God created great whales, and every living creature that moveth, which the waters brought forth abundantly, after their kind, and every winged fowl after his kind: and God saw that it was good."
+}
+if(verse===22){
+return "And God blessed them, saying, Be fruitful, and multiply, and fill the waters in the seas, and let fowl multiply in the earth."
+}
+if(verse===23){
+return "And the evening and the morning were the fifth day."
+}
+if(verse===24){
+return "And God said, Let the earth bring forth the living creature after his kind, cattle, and creeping thing, and beast of the earth after his kind: and it was so."
+}
+if(verse===25){
+return "And God made the beast of the earth after his kind, and cattle after their kind, and every thing that creepeth upon the earth after his kind: and God saw that it was good."
+}
+if(verse===26){
+return " And God said, Let us make man in our image, after our likeness: and let them have dominion over the fish of the sea, and over the fowl of the air, and over the cattle, and over all the earth, and over every creeping thing that creepeth upon the earth."
+}
+if(verse===27){
+return "So God created man in his own image, in the image of God created he him; male and female created he them."
+}
+if(verse===28){
+return "And God blessed them, and God said unto them, Be fruitful, and multiply, and replenish the earth, and subdue it: and have dominion over the fish of the sea, and over the fowl of the air, and over every living thing that moveth upon the earth."
+}
+if(verse===29){
+return "And God said, Behold, I have given you every herb bearing seed, which is upon the face of all the earth, and every tree, in the which is the fruit of a tree yielding seed; to you it shall be for meat."
+}
+if(verse===30){
+return "And to every beast of the earth, and to every fowl of the air, and to every thing that creepeth upon the earth, wherein there is life, I have given every green herb for meat: and it was so."
+}
+if(verse===31){
+return "And God saw every thing that he had made, and, behold, it was very good. And the evening and the morning were the sixth day."
+}
+}
+if(chapter===2){
+return chapter2[verse]
+}
+}
