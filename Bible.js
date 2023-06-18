626edf338df7495c5d3e01096010acfa277ed942
achristianonamission <132952751+achristianonamission@users.noreply.github.com>
2023-06-17 23:07:02 -0400

    Bible.js

--- Bible.js
+++ Bible.js
@@ -2,7 +2,19 @@ function load(book)
 {
 if(book==="Genesis"){
 var script=document.createElement("script")
-script.src="https://cdn.jsdelivr.net/gh/achristianonamission/Bible-API@main/Genesis.js"
+script.src="https://cdn.jsdelivr.net/gh/achristianonamission/Bible-API@Genesis/G1.js"
+document.head.appendChild(script)
+  var script=document.createElement("script")
+script.src="https://cdn.jsdelivr.net/gh/achristianonamission/Bible-API@Genesis/G2.js"
+document.head.appendChild(script)
+  var script=document.createElement("script")
+script.src="https://cdn.jsdelivr.net/gh/achristianonamission/Bible-API@Genesis/G3.js"
+document.head.appendChild(script)
+  var script=document.createElement("script")
+script.src="https://cdn.jsdelivr.net/gh/achristianonamission/Bible-API@Genesis/G4.js"
+document.head.appendChild(script)
+  var script=document.createElement("script")
+script.src="https://cdn.jsdelivr.net/gh/achristianonamission/Bible-API@Genesis/G5.js"
 document.head.appendChild(script)
 }
 }
