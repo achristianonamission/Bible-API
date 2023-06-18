bf3e2bfc13d3eef5343904e342c51ef1a565da15
achristianonamission <132952751+achristianonamission@users.noreply.github.com>
2023-06-18 17:23:32 -0400

    Add files via upload

---
+++ G6.js
@@ -0,0 +1,68 @@
+function Genesis6(verse){
+if(verse===1){
+return "And it came to pass, when men began to multiply on the face of the earth, and daughters were born unto them,"
+}
+if(verse===2){
+return "That the sons of God saw the daughters of men that they were fair; and they took them wives of all which they chose."
+}
+if(verse===3){
+return "And the Lord said, My spirit shall not always strive with man, for that he also is flesh: yet his days shall be an hundred and twenty years."
+}
+if(verse===4){
+return "There were giants in the earth in those days; and also after that, when the sons of God came in unto the daughters of men, and they bare children to them, the same became mighty men which were of old, men of renown."
+}
+if(verse===5){
+return "And God saw that the wickedness of man was great in the earth, and that every imagination of the thoughts of his heart was only evil continually."
+}
+if(verse===6){
+return "And it repented the Lord that he had made man on the earth, and it grieved him at his heart."
+}
+if(verse===7){
+return "And the Lord said, I will destroy man whom I have created from the face of the earth; both man, and beast, and the creeping thing, and the fowls of the air; for it repenteth me that I have made them."
+}
+if(verse===8){
+return "But Noah found grace in the eyes of the Lord."
+}
+if(verse===9){
+return "These are the generations of Noah: Noah was a just man and perfect in his generations, and Noah walked with God."
+}
+if(verse ===10){
+return "And Noah begat three sons, Shem, Ham, and Japheth."
+}
+if(verse===11){
+return "The earth also was corrupt before God, and the earth was filled with violence."
+}
+if(verse===12){
+return "And God looked upon the earth, and, behold, it was corrupt; for all flesh had corrupted his way upon the earth."
+}
+if(verse===13){
+return "And God said unto Noah, The end of all flesh is come before me; for the earth is filled with violence through them; and, behold, I will destroy them with the earth."
+}
+if(verse===14){
+return "Make thee an ark of gopher wood; rooms shalt thou make in the ark, and shalt pitch it within and without with pitch."
+}
+if(verse===15){
+return "And this is the fashion which thou shalt make it of: The length of the ark shall be three hundred cubits, the breadth of it fifty cubits, and the height of it thirty cubits."
+}
+if(verse===16){
+return "A window shalt thou make to the ark, and in a cubit shalt thou finish it above; and the door of the ark shalt thou set in the side thereof; with lower, second, and third stories shalt thou make it."
+}
+if(verse===17){
+return "And, behold, I, even I, do bring a flood of waters upon the earth, to destroy all flesh, wherein is the breath of life, from under heaven; and every thing that is in the earth shall die."
+}
+if(verse===18){
+return "But with thee will I establish my covenant; and thou shalt come into the ark, thou, and thy sons, and thy wife, and thy sons' wives with thee."
+}
+if(verse===19){
+return "And of every living thing of all flesh, two of every sort shalt thou bring into the ark, to keep them alive with thee; they shall be male and female."
+}
+if(verse===20){
+return "Of fowls after their kind, and of cattle after their kind, of every creeping thing of the earth after his kind, two of every sort shall come unto thee, to keep them alive."
+}
+if(verse===21){
+return "And take thou unto thee of all food that is eaten, and thou shalt gather it to thee; and it shall be for food for thee, and for them."
+}
+if(verse===22){
+return "Thus did Noah; according to all that God commanded him, so did he."
+}
+}
