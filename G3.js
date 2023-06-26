65747c9fe9df01acdfc215dace05a2c69ced4740
achristianonamission <132952751+achristianonamission@users.noreply.github.com>
2023-06-25 20:20:50 -0400

    Add files via upload

--- G3.js
+++ G3.js
@@ -1,77 +1,74 @@
 function Genesis3(verse){
 if(verse===1){
-return "Thus the heavens and the earth were finished, and all the host of them."
+return "Now the serpent was more subtil than any beast of the field which the Lord God had made. And he said unto the woman, Yea, hath God said, Ye shall not eat of every tree of the garden?"
 }
 if(verse===2){
-return "And on the seventh day God ended his work which he had made; and he rested on the seventh day from all his work which he had made."
+return "And the woman said unto the serpent, We may eat of the fruit of the trees of the garden:"
 }
 if(verse===3){
-return "And God blessed the seventh day, and sanctified it: because that in it he had rested from all his work which God created and made."
+return "But of the fruit of the tree which is in the midst of the garden, God hath said, Ye shall not eat of it, neither shall ye touch it, lest ye die."
 }
 if(verse===4){
-return "These are the generations of the heavens and of the earth when they were created, in the day that the Lord God made the earth and the heavens,"
+return "And the serpent said unto the woman, Ye shall not surely die:"
 }
 if(verse===5){
-return "And every plant of the field before it was in the earth, and every herb of the field before it grew: for the Lord God had not caused it to rain upon the earth, and there was not a man to till the ground."
+return "For God doth know that in the day ye eat thereof, then your eyes shall be opened, and ye shall be as gods, knowing good and evil."
 }
 if(verse===6){
-return "But there went up a mist from the earth, and watered the whole face of the ground."
+return "And when the woman saw that the tree was good for food, and that it was pleasant to the eyes, and a tree to be desired to make one wise, she took of the fruit thereof, and did eat, and gave also unto her husband with her; and he did eat."
 }
 if(verse===7){
-return "And the Lord God formed man of the dust of the ground, and breathed into his nostrils the breath of life; and man became a living soul."
+return "And the eyes of them both were opened, and they knew that they were naked; and they sewed fig leaves together, and made themselves aprons."
 }
 if(verse===8){
-return "And the Lord God planted a garden eastward in Eden; and there he put the man whom he had formed."
+return "And they heard the voice of the Lord God walking in the garden in the cool of the day: and Adam and his wife hid themselves from the presence of the Lord God amongst the trees of the garden."
 }
 if(verse===9){
-return "And out of the ground made the Lord God to grow every tree that is pleasant to the sight, and good for food; the tree of life also in the midst of the garden, and the tree of knowledge of good and evil."
+return "And the Lord God called unto Adam, and said unto him, Where art thou?"
 }
 if(verse===10){
-return "And a river went out of Eden to water the garden; and from thence it was parted, and became into four heads."
+return "And he said, I heard thy voice in the garden, and I was afraid, because I was naked; and I hid myself."
 }
 if(verse===11){
-return "The name of the first is Pison: that is it which compasseth the whole land of Havilah, where there is gold;"
+return "And he said, Who told thee that thou wast naked? Hast thou eaten of the tree, whereof I commanded thee that thou shouldest not eat?"
 }
 if(verse===12){
-return "And the gold of that land is good: there is bdellium and the onyx stone."
+return "And the man said, The woman whom thou gavest to be with me, she gave me of the tree, and I did eat."
 }
 if(verse===13){
-return "And the name of the second river is Gihon: the same is it that compasseth the whole land of Ethiopia."
+return "And the Lord God said unto the woman, What is this that thou hast done? And the woman said, The serpent beguiled me, and I did eat."
 }
 if(verse===14){
-return "And the name of the third river is Hiddekel: that is it which goeth toward the east of Assyria. And the fourth river is Euphrates."
+return "And the Lord God said unto the serpent, Because thou hast done this, thou art cursed above all cattle, and above every beast of the field; upon thy belly shalt thou go, and dust shalt thou eat all the days of thy life:"
 }
 if(verse===15){
-return "And the Lord God took the man, and put him into the garden of Eden to dress it and to keep it."
+return "And I will put enmity between thee and the woman, and between thy seed and her seed; it shall bruise thy head, and thou shalt bruise his heel."
 }
 if(verse===16){
-return "And the Lord God commanded the man, saying, Of every tree of the garden thou mayest freely eat:"
+return "Unto the woman he said, I will greatly multiply thy sorrow and thy conception; in sorrow thou shalt bring forth children; and thy desire shall be to thy husband, and he shall rule over thee."
 }
 if(verse===17){
-return "But of the tree of the knowledge of good and evil, thou shalt not eat of it: for in the day that thou eatest thereof thou shalt surely die."
+return "And unto Adam he said, Because thou hast hearkened unto the voice of thy wife, and hast eaten of the tree, of which I commanded thee, saying, Thou shalt not eat of it: cursed is the ground for thy sake; in sorrow shalt thou eat of it all the days of thy life;"
 }
 if(verse===18){
-return "And the Lord God said, It is not good that the man should be alone; I will make him an help meet for him."
+return "Thorns also and thistles shall it bring forth to thee; and thou shalt eat the herb of the field;"
 }
 if(verse===19){
-return "And out of the ground the Lord God formed every beast of the field, and every fowl of the air; and brought them unto Adam to see what he would call them: and whatsoever Adam called every living creature, that was the name thereof."
+return " In the sweat of thy face shalt thou eat bread, till thou return unto the ground; for out of it wast thou taken: for dust thou art, and unto dust shalt thou return."
 }
 if(verse===20){
-return "And Adam gave names to all cattle, and to the fowl of the air, and to every beast of the field; but for Adam there was not found an help meet for him."
+return "And Adam called his wife's name Eve; because she was the mother of all living."
 }
 if(verse===21){
-return "And the Lord God caused a deep sleep to fall upon Adam, and he slept: and he took one of his ribs, and closed up the flesh instead thereof;"
+return "Unto Adam also and to his wife did the Lord God make coats of skins, and clothed them."
 }
 if(verse===22){
-return "And the rib, which the Lord God had taken from man, made he a woman, and brought her unto the man."
+return "And the Lord God said, Behold, the man is become as one of us, to know good and evil: and now, lest he put forth his hand, and take also of the tree of life, and eat, and live for ever:"
 }
 if(verse===23){
-return "Adam said, This is now bone of my bones, and flesh of my flesh: she shall be called Woman, because she was taken out of Man."
+return "Therefore the Lord God sent him forth from the garden of Eden, to till the ground from whence he was taken."
 }
 if(verse===24){
-return "Therefore shall a man leave his father and his mother, and shall cleave unto his wife: and they shall be one flesh."
-}
-if(verse===25){
-return "And they were both naked, the man and his wife, and were not ashamed."
+return "So he drove out the man; and he placed at the east of the garden of Eden Cherubims, and a flaming sword which turned every way, to keep the way of the tree of life."
 }
 }
