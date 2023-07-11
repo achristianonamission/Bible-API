705a9a44518b4d231fb8489b4701e4dac3285abf
achristianonamission <132952751+achristianonamission@users.noreply.github.com>
2023-07-10 20:57:26 -0500

    Add files via upload

---
+++ G7.js
@@ -0,0 +1,77 @@
+function Genesis7(verse){
+if(verse===1){
+return "And the Lord said unto Noah, Come thou and all thy house into the ark; for thee have I seen righteous before me in this generation."
+}
+if(verse===2){
+return "Of every clean beast thou shalt take to thee by sevens, the male and his female: and of beasts that are not clean by two, the male and his female."
+}
+if(verse===3){
+return "Of fowls also of the air by sevens, the male and the female; to keep seed alive upon the face of all the earth."
+}
+if(verse===4){
+return "For yet seven days, and I will cause it to rain upon the earth forty days and forty nights; and every living substance that I have made will I destroy from off the face of the earth."
+}
+if(verse===5){
+return "And Noah did according unto all that the Lord commanded him."
+}
+if(verse===6){
+return "And Noah was six hundred years old when the flood of waters was upon the earth."
+}
+if(verse===7){
+return "And Noah went in, and his sons, and his wife, and his sons' wives with him, into the ark, because of the waters of the flood."
+}
+if(verse===8){
+return "Of clean beasts, and of beasts that are not clean, and of fowls, and of every thing that creepeth upon the earth,"
+}
+if(verse===9){
+return "There went in two and two unto Noah into the ark, the male and the female, as God had commanded Noah."
+}
+if(verse===10){
+return "And it came to pass after seven days, that the waters of the flood were upon the earth."
+}
+if(verse===11){
+return "In the six hundredth year of Noah's life, in the second month, the seventeenth day of the month, the same day were all the fountains of the great deep broken up, and the windows of heaven were opened."
+}
+if(verse===12){
+return "And the rain was upon the earth forty days and forty nights."
+}
+if(verse===13){
+return "In the selfsame day entered Noah, and Shem, and Ham, and Japheth, the sons of Noah, and Noah's wife, and the three wives of his sons with them, into the ark;"
+}
+if(verse===14){
+return "They, and every beast after his kind, and all the cattle after their kind, and every creeping thing that creepeth upon the earth after his kind, and every fowl after his kind, every bird of every sort."
+}
+if(verse===15){
+return "And they went in unto Noah into the ark, two and two of all flesh, wherein is the breath of life."
+}
+if(verse===16){
+return "And they that went in, went in male and female of all flesh, as God had commanded him: and the Lord shut him in."
+}
+if(verse===17){
+return "And the flood was forty days upon the earth; and the waters increased, and bare up the ark, and it was lift up above the earth."
+}
+if(verse===18){
+return "And the waters prevailed, and were increased greatly upon the earth; and the ark went upon the face of the waters."
+}
+if(verse===19){
+return "And the waters prevailed exceedingly upon the earth; and all the high hills, that were under the whole heaven, were covered."
+}
+if(verse===20){
+return "Fifteen cubits upward did the waters prevail; and the mountains were covered."
+}
+if(verse===21){
+return "And all flesh died that moved upon the earth, both of fowl, and of cattle, and of beast, and of every creeping thing that creepeth upon the earth, and every man:"
+}
+if(verse===22){
+return "All in whose nostrils was the breath of life, of all that was in the dry land, died."
+}
+if(verse===23){
+return "And every living substance was destroyed which was upon the face of the ground, both man, and cattle, and the creeping things, and the fowl of the heaven; and they were destroyed from the earth: and Noah only remained alive, and they that were with him in the ark."
+}
+if(verse===24){
+return "And the waters prevailed upon the earth an hundred and fifty days."
+}
+if(verse>24){
+return "Not valid in this chapter"
+}
+}
