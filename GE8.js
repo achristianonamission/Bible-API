f66642f5538c7b2133dd8d43aa230f1f615ee56a
achristianonamission <132952751+achristianonamission@users.noreply.github.com>
2023-07-11 17:29:37 -0500

    Add files via upload

---
+++ GE8.js
@@ -0,0 +1,88 @@
+document.getElementById("gc8v1").onclick=function(){
+document.getElementById("gc8v1").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 1")
+}
+document.getElementById("gc8v2").onclick=function(){
+document.getElementById("gc8v2").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 2")
+}
+document.getElementById("gc8v3").onclick=function(){
+document.getElementById("gc8v3").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 3")
+}
+document.getElementById("gc8v4").onclick=function(){
+document.getElementById("gc8v4").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 4")
+}
+document.getElementById("gc8v5").onclick=function(){
+document.getElementById("gc8v5").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 5")
+}
+document.getElementById("gc8v6").onclick=function(){
+document.getElementById("gc8v6").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 6")
+}
+document.getElementById("gc8v7").onclick=function(){
+document.getElementById("gc8v7").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 7")
+}
+document.getElementById("gc8v8").onclick=function(){
+document.getElementById("gc8v8").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 8")
+}
+document.getElementById("gc8v9").onclick=function(){
+document.getElementById("gc8v9").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 9")
+}
+document.getElementById("gc8v10").onclick=function(){
+document.getElementById("gc8v10").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 10")
+}
+document.getElementById("gc8v11").onclick=function(){
+document.getElementById("gc8v11").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 11")
+}
+document.getElementById("gc8v12").onclick=function(){
+document.getElementById("gc8v12").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 12")
+}
+document.getElementById("gc8v13").onclick=function(){
+document.getElementById("gc8v13").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 13")
+}
+document.getElementById("gc8v14").onclick=function(){
+document.getElementById("gc8v14").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 14")
+}
+document.getElementById("gc8v15").onclick=function(){
+document.getElementById("gc8v15").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 15")
+}
+document.getElementById("gc8v16").onclick=function(){
+document.getElementById("gc8v16").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 16")
+}
+document.getElementById("gc8v17").onclick=function(){
+document.getElementById("gc8v17").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 17")
+}
+document.getElementById("gc8v18").onclick=function(){
+document.getElementById("gc8v18").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 18")
+}
+document.getElementById("gc8v19").onclick=function(){
+document.getElementById("gc8v19").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 19")
+}
+document.getElementById("gc8v20").onclick=function(){
+document.getElementById("gc8v20").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 20")
+}
+document.getElementById("gc8v21").onclick=function(){
+document.getElementById("gc8v21").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 21")
+}
+document.getElementById("gc8v22").onclick=function(){
+document.getElementById("gc8v22").style.backgroundColor="green"
+localStorage.setItem("bookmark","Genesis chapter 8 verse 22")
+}
