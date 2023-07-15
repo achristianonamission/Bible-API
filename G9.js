07b84f0753c16d13b43dac47d686027bfd9691d0
achristianonamission <132952751+achristianonamission@users.noreply.github.com>
2023-07-14 19:57:44 -0500

    Add files via upload

---
+++ G9.js
@@ -0,0 +1,89 @@
+function Genesis9(verse){
+if(verse===1){
+return "And God blessed Noah and his sons, and said unto them, Be fruitful, and multiply, and replenish the earth."
+}
+if(verse ===2){
+return "And the fear of you and the dread of you shall be upon every beast of the earth, and upon every fowl of the air, upon all that moveth upon the earth, and upon all the fishes of the sea; into your hand are they delivered."
+}
+if(verse===3){
+return "Every moving thing that liveth shall be meat for you; even as the green herb have I given you all things."
+}
+if(verse===4){
+return "But flesh with the life thereof, which is the blood thereof, shall ye not eat."
+}
+if(verse===5){
+return "And surely your blood of your lives will I require; at the hand of every beast will I require it, and at the hand of man; at the hand of every man's brother will I require the life of man."
+}
+if(verse===6){
+return "Whoso sheddeth man's blood, by man shall his blood be shed: for in the image of God made he man."
+}
+if(verse===7){
+return "And you, be ye fruitful, and multiply; bring forth abundantly in the earth, and multiply therein."
+}
+if(verse===8){
+return "And God spake unto Noah, and to his sons with him, saying,"
+}
+if(verse===9){
+return "And I, behold, I establish my covenant with you, and with your seed after you;"
+}
+if(verse===10){
+return "And with every living creature that is with you, of the fowl, of the cattle, and of every beast of the earth with you; from all that go out of the ark, to every beast of the earth."
+}
+if(verse===11){
+return "And I will establish my covenant with you, neither shall all flesh be cut off any more by the waters of a flood; neither shall there any more be a flood to destroy the earth."
+}
+if(verse ===12){
+return "And God said, This is the token of the covenant which I make between me and you and every living creature that is with you, for perpetual generations:"
+}
+if(verse===13){
+return "I do set my bow in the cloud, and it shall be for a token of a covenant between me and the earth."
+}
+if(verse===14){
+return "And it shall come to pass, when I bring a cloud over the earth, that the bow shall be seen in the cloud:"
+}
+if(verse===15){
+return "And I will remember my covenant, which is between me and you and every living creature of all flesh; and the waters shall no more become a flood to destroy all flesh."
+}
+if(verse===16){
+return "And the bow shall be in the cloud; and I will look upon it, that I may remember the everlasting covenant between God and every living creature of all flesh that is upon the earth."
+}
+if(verse===17){
+return "And God said unto Noah, This is the token of the covenant, which I have established between me and all flesh that is upon the earth."
+}
+if(verse===18){
+return "And the sons of Noah, that went forth of the ark, were Shem, and Ham, and Japheth: and Ham is the father of Canaan."
+}
+if(verse===19){
+return "These are the three sons of Noah: and of them was the whole earth overspread."
+}
+if(verse===20){
+return "And Noah began to be an husbandman, and he planted a vineyard:"
+}
+if(verse===21){
+return "And he drank of the wine, and was drunken; and he was uncovered within his tent."
+}
+if(verse===22){
+return "And Ham, the father of Canaan, saw the nakedness of his father, and told his two brethren without."
+}
+if(verse===23){
+return "And Shem and Japheth took a garment, and laid it upon both their shoulders, and went backward, and covered the nakedness of their father; and their faces were backward, and they saw not their father's nakedness."
+}
+if(verse===24){
+return "And Noah awoke from his wine, and knew what his younger son had done unto him."
+}
+if(verse===25){
+return "And he said, Cursed be Canaan; a servant of servants shall he be unto his brethren."
+}
+if(verse===26){
+return "And he said, Blessed be the Lord God of Shem; and Canaan shall be his servant."
+}
+if(verse===27){
+return "God shall enlarge Japheth, and he shall dwell in the tents of Shem; and Canaan shall be his servant."
+}
+if(verse===28){
+return "And Noah lived after the flood three hundred and fifty years."
+}
+if(verse===29){
+return "And all the days of Noah were nine hundred and fifty years: and he died."
+}
+}
